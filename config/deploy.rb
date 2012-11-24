de30fcba88c51ab70ef2d8c174e6c494581f72e0
Santthosh <santthosh@appinmap.com>
2012-11-23 17:12:15 -0800

    Fixing the deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,25 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'capistrano/ext/multistage'
+require "rvm/capistrano"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :normalize_asset_timestamps, false
+set :stages, ["development","staging", "production"]
+set :default_stage, "development"
+set :bundle_without, [:darwin, :development, :test]
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :rvm_type, :user
+set :rvm_ruby_string, 'ruby-1.9.2-p318@workers'
+
+set :scm, :git
+set :scm_passphrase, ""
+set :application, "brightpush-aim-sync"
+set :deploy_to, "/var/www/brightpush-aim-sync"
+set :repository,  "git@bright.unfuddle.com:bright/brightpush-aim-sync.git"
+set :user, "ubuntu"
+set :rack_env,"development"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -20,3 +32,14 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+ 
+ before 'deploy', 'rvm:install_ruby'
+ 
+ after 'deploy:update_code', 'deploy:start_workers'
+ namespace :deploy do
+  desc "Starts the workers"
+  task :start_workers, :roles => :app do
+    run "cd #{release_path} && bundle install"
+    run "cd #{release_path} && scripts/workers restart #{rack_env}"
+  end
+ end
