6448509b7de9eb40677d80d616949c75c4b3a31b
Santthosh <santthosh@appinmap.com>
2012-11-25 04:09:46 -0800

    Refactoring to aim_token_synchronization

--- spec/aim_token_synchronization_spec.rb
+++ spec/aim_token_synchronization_spec.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + '/../lib/aim_token_synchronization.rb'
 
 describe "AIM_Token_Synchronization" do
   it "should provide functionality for synchronizing new and updated iOS device tokens to brightpush" do
-    AIM_iOS_Synchronization.method_defined?(:perform)
+    AIM_Token_Synchronization.method_defined?(:perform)
   end
 end
